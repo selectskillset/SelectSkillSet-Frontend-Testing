56d72360d072e65f9b9ed9e38638c6af3727bc61
suleman <selectskillset@gmail.com>
2025-04-21 16:42:22 +0530

    removed career cloud

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -4,7 +4,7 @@ import Features from "./Features";
 import CallToAction from "./CallToAction";
 // import SliderCards from "./SliderCards";
 import AboutUsComponent from "./AboutUsComponent";
-import CareerCloud from "./CareerCloud";
+// import CareerCloud from "./CareerCloud";
 
 export const HomePage = () => {
   return (
@@ -12,7 +12,7 @@ export const HomePage = () => {
       <Hero />
       {/*<LandingFeature />*/}
       <AboutUsComponent />
-      <CareerCloud/>
+      {/* <CareerCloud/> */}
       {/* <SliderCards /> */}
       <Features />
       {/* <Statistics /> */}
